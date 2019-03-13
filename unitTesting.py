a464faf3b949c033c7c776dd77c3b770c0100924
Tech_Programming <37150682+SynergyTech75@users.noreply.github.com>
2019-03-13 10:51:13 +0800

    Testing with a single testing main

---
+++ unitTesting.py
@@ -0,0 +1,8 @@
+import unittest
+from app.testingClasses.TestPallet import TestPallet
+from app.testingClasses.TestContents import TestContents
+from app.testingClasses.TestCustomer import TestCustomer
+from app.testingClasses.TestDelivery import TestDelivery
+
+if __name__ == '__main__':
+    unittest.main()
